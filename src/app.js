6f7f74886d073fd93dc81499061ba097645e850b
barbosa92 <alexrp_92@hotmail.com>
2022-03-25 19:26:46 +0000

    Generador de excusas acabado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let generateExcuse = () => {
+  let pronoun = ["Un"];
+  let subject = ["runner", "perro", "payaso", "jigglypuff"];
+  let action = [
+    "me ha quitado",
+    "se ha comido",
+    "se ha llevado",
+    "se ha ido volando en mi"
+  ];
+  let possesion = ["portatil", "bicicleta", "cuaderno", "movil"];
+  let where = ["en el parque", "durante el partido", "en el instituto"];
 
-window.onload = function() {
-  document;
-  console.log("Hello Rigo from the console!");
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possesionIndex = Math.floor(Math.random() * possesion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possesion[possesionIndex] +
+    " " +
+    where[whereIndex]
+  );
+};
+
+window.onload = () => {
+  let randomNumber = Math.random() * 10;
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = generateExcuse();
+  });
+
+  console.log("Hello Rigo from the console!" + randomNumber);
 };
